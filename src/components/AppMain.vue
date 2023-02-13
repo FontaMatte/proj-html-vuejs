7f3bb1ac3dabcb91a32e4f004e35050ed2b3dd77
FontaMatte <matteo.fontanel@gmail.com>
2023-02-13 18:28:56 +0100

    Main layout completed

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -62,12 +62,256 @@ export default {
             <div class="card">CARD 5</div>
           </div>    
         </div>
-      </div>
-      
-      
+      </div> 
     </section>
 
-    
+    <!-- SECTION 3 -->
+    <section class="section-3">
+      <div class="container">
+        <div class="section-image">
+          <img src="" alt="">
+          IMG
+        </div>
+        <div class="section-text">
+          <h3>
+            Law Faculty
+          </h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Quasi voluptates tempore cum ab odio reprehenderit nemo dolorem deleniti quod iste!
+          </p>
+          <button>Read More</button>
+        </div>
+      </div>
+    </section>
+
+    <!-- SECTION 4 -->
+    <section class="section-4">
+      <div class="container">
+        <div class="section-top">
+          <img src="" alt="">
+          <h2>
+            University Year
+          </h2>
+        </div>
+        <div class="section-bottom">
+          <img class="background-image" src="" alt="">
+          <div class="on-image-text">
+            <div class="Text-1">
+              <h4>
+                Orentation
+              </h4>
+              <p>
+                First day of the university year, all students gather for the opening
+                cerimony and then network with others.
+              </p>
+            </div>
+            <div class="Text-2">
+              <h4>
+                Demo Classe
+              </h4>
+              <p>
+                In the first week, students try to accomodate with the teaching style and choose 
+                their optional courses.
+              </p>
+            </div>
+            <div class="Text-3">
+              <h4>
+                Evaluation
+              </h4>
+              <p>
+                At the end of a semester, student take a genmeral evaluation test for every subject they're learning.
+              </p>
+            </div>
+            <div class="Text-4">
+              <h4>
+                Graduation Day
+              </h4>
+              <p>
+                On the day of graduation, all students gather for the ceremony and then network and party among others.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- SECTION 5 -->
+    <section class="section-5">
+      <div class="container">
+        <div class="section-top">
+          <img src="" alt="">
+          <h2>
+            Upcoming Events
+          </h2>
+        </div>
+        <div class="section-bottom">
+          <div class="card-container">
+            <div class="card">
+              <h4>Coaching Session</h4>
+              <span>20 May 21:30 PM</span>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, tenetur.
+              </p>
+              <button>+ Find More</button>
+            </div>
+            <div class="card">
+              <h4>Coaching Session</h4>
+              <span>24 Mar 18:00 PM</span>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, tenetur.
+              </p>
+              <button>+ Find More</button>
+            </div>
+            <div class="card">
+              <h4>Coaching Session</h4>
+              <span>12 Feb 13:30 PM</span>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, tenetur.
+              </p>
+              <button>+ Find More</button>
+            </div>
+          </div>
+          <button class="view-all-button">View All Events</button>
+        </div>
+      </div>
+    </section>
+
+    <!-- SECTION 6 -->
+  <section class="section-6">
+    <div class="container">
+      <h2>
+        Latest Courses
+      </h2>
+      <div class="card-container">
+        <!-- CARD -->
+        <div class="card">
+          <div class="card-img">
+            <img src="" alt="">
+          </div>
+          <div class="card-text">
+            <h5>
+              Make Better Decision
+            </h5>
+            <span>Teacher:<span> James Collins</span></span>
+            <span>Price:<span>$21.00</span></span>
+          </div>
+          <button>View Course</button>
+        </div>
+        <div class="card">
+          <div class="card-img">
+            <img src="" alt="">
+          </div>
+          <div class="card-text">
+            <h5>
+              How to be a speaker
+            </h5>
+            <span>Teacher:<span> James Collins</span></span>
+            <span>Price:<span>Free</span></span>
+          </div>
+          <button>View Course</button>
+        </div><div class="card">
+          <div class="card-img">
+            <img src="" alt="">
+          </div>
+          <div class="card-text">
+            <h5>
+              Network introduction
+            </h5>
+            <span>Teacher:<span> James Collins</span></span>
+            <span>Price:<span>Free</span></span>
+          </div>
+          <button>View Course</button>
+        </div><div class="card">
+          <div class="card-img">
+            <img src="" alt="">
+          </div>
+          <div class="card-text">
+            <h5>
+              Brand Managment
+            </h5>
+            <span>Teacher:<span> James Collins</span></span>
+            <span>Price:<span>Free</span></span>
+          </div>
+          <button>View Course</button>
+        </div>
+      </div>
+    </div>
+  </section>   
+  
+  <!-- SECTION 7 -->
+  <section class="section-7">
+    <div class="container">
+      <div class="text-section">
+        <img src="" alt="">
+        <h2>
+          The most efficient examination method
+        </h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+          Quam aperiam provident cum omnis illo, distinctio natus dolorum autem eligendi eveniet.
+        </p>
+        <button>Discover the Method</button>
+      </div>
+      <div class="img-section">
+        <img src="" alt="">
+        IMG
+      </div>
+    </div>
+  </section>
+
+  <!-- SECTION 8 -->
+  <section class="section-8">
+    <div class="container">
+      <div class="img-section">
+        <img src="" alt="">
+        IMG
+      </div>
+      <div class="text-section">
+        <img src="" alt="">
+        <h2>
+          Variable fess fpr international students
+        </h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+          Quam aperiam provident cum omnis illo, distinctio natus dolorum autem eligendi eveniet.
+        </p>
+        <button>List of fees</button>
+      </div>    
+    </div>
+  </section>
+
+  <!-- SECTION 9 -->
+  <section class="section-9">
+    <div class="container">
+      <h2>Subscribe Now to Our Newsletter !</h2>
+      <div class="email-input">
+        <input type="email" placeholder="Email address....">
+        <button>GO</button>
+      </div>
+    </div>
+  </section>
+
+  <!-- SECTION 10 -->
+  <section class="section-10">
+    <div class="container">
+      <h2>
+        Partners
+      </h2>
+      <p>
+        Leverage agile frameworks to provide a robust synipsin for high level overviews.
+        Iterative approaches to corporate strategy.
+      </p>
+      <div class="img-container">
+        <img src="" alt="logo1">
+        <img src="" alt="logo2">
+        <img src="" alt="logo3">
+        <img src="" alt="logo4">
+        <img src="" alt="logo5">
+        <img src="" alt="logo6">
+      </div>
+    </div>
+  </section>
   </main>
 </template>
 
