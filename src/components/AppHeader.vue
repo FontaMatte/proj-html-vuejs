1cb7e5c93512b224fa2a2990a1e38d83df57564c
FontaMatte <matteo.fontanel@gmail.com>
2023-02-13 16:30:05 +0100

    Header + fisrt 2 section layout completed

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -16,7 +16,45 @@ export default {
 
 <template>
   <header>
-    HEADER
+    <!-- HEADER TOP -->
+    <div class="header-top">
+      <div class="main-logo">
+        LOGO + TITLE
+      </div>
+      <div class="nav-bar">
+        <ul>
+          <li><a href="#"></a>Home</li>
+          <li><a href="#"></a>Home</li>
+          <li><a href="#"></a>Home</li>
+          <li><a href="#"></a>Home</li>
+          <li><a href="#"></a>Home</li>
+          <li><a href="#"></a>Home</li>
+        </ul>
+        <button>VIEW COURSES</button>
+      </div>
+    </div>
+
+    <!-- HEADER BOTTOM -->
+    <div class="header-bottom">
+      <div class="aside-menu">
+        <img src="" alt="">
+        <img src="" alt="">
+        <img src="" alt="">
+        ASIDE MENU
+      </div>
+      <div class="container">
+        <div class="header-bottom-content">
+          <h1>
+            Key to your succes
+          </h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Voluptatem sequi nobis quod quis nostrum dicta.
+          </p>
+          <button>Search courses</button>
+          <button>Apply for university</button>
+        </div>
+      </div>
+    </div>
   </header>
 </template>
 
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -16,7 +16,58 @@ export default {
 
 <template>
   <main>
-    MAIN
+
+    <!-- SECTION 1 -->
+    <section class="section-1">
+      <div class="call-to-action-link"></div>
+      <div class="background-image">
+        <img src="" alt="">
+      </div>
+      <div class="container">
+        <div class="section-text">
+          <img src="" alt="">
+          <h2>
+            Thousands of courses for any type of student
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Nostrum id, nulla veniam laudantium enim velit.
+          </p>
+          <button>Browse through courses</button>
+        </div>
+        <div class="section-image">
+          <img src="" alt="">
+          IMAGE
+        </div>
+      </div>
+    </section>
+
+    <!-- SECTION 2 -->
+    <section class="section-2">
+      <div class="container">
+        <div class="section-top">
+          <h2>
+            Faculties aviable at EduPrime
+          </h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, culpa.</p>
+        </div>
+      </div>
+      <div class="section-bottom">
+        <div class="container">
+          <div class="card-container">
+            <div class="card">CARD 1</div>
+            <div class="card">CARD 2</div>
+            <div class="card">CARD 3</div>
+            <div class="card">CARD 4</div>
+            <div class="card">CARD 5</div>
+          </div>    
+        </div>
+      </div>
+      
+      
+    </section>
+
+    
   </main>
 </template>
 
